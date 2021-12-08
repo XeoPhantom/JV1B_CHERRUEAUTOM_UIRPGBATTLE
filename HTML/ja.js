85436b94d4da69383b9017964324ca94c58c471f
Tom Ch <74670050+XeoPhantom@users.noreply.github.com>
2021-12-08 22:36:18 +0100

    encore

---
+++ HTML/ja.js
@@ -0,0 +1,12 @@
+document.getElementById("RPGBattlesansannimation").innerHTML = "Hello JavaScript";
+
+var hp = 100 ;
+var hattaque = 50;
+var shield = 5;
+
+var hplapin = 666;
+var attaquelapin =10;
+
+Attaquebutton.onclick = function() {
+    hplapin = hattaque - hplapin;
+}
