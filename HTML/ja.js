023d21c43ad6ee194019eca8d9c5e745b1c1be60
Tom Ch <74670050+XeoPhantom@users.noreply.github.com>
2021-12-11 19:03:05 +0100

    11/12/2021

--- HTML/ja.js
+++ HTML/ja.js
@@ -9,4 +9,22 @@ var attaquelapin =10;
 
 Attaquebutton.onclick = function() {
     hplapin = hattaque - hplapin;
+    "les Monstres contre attaques!"
+    hp = hp - attaquelapin 
+
+}
+DefenseButton.onclick = function() {
+    hp = hp + shield
+    "les Monstres contre attaques!"
+    hp = hp - attaquelapin 
+
+}
+Ultime.onclick = function() {
+  hp = hp - 50
+  "vous sacrifier voyre vie en echange d'une double attaque"
+  hattaque = hattaque * 2
+  hplapin = hplapin - hattaque
+  hattaque / 2 
+  "les Monstres contre attaques!"
+    hp = hp - attaquelapin
 }
